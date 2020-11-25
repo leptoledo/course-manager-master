1c71ae9a90a5b3e4bf8b943de2777f2714dd94e2
Leandro Toledo <leandrotoledo@hotmail.com.br>
2020-11-25 16:15:39 +0000

    aplicando rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { CourseListComponent } from './courses/course-list.component';
+import { StarComponent } from 'src/star/star.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { Error404Component } from './error-404/error-404.component';
+import { CourseInfoComponent } from './courses/course-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseListComponent,
+    StarComponent,
+    NavBarComponent,
+    Error404Component,
+    CourseInfoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'courses', component: CourseListComponent
+      },
+      {
+        path: 'course/info', component: CourseInfoComponent
+      },
+      {
+        path: '', redirectTo: 'courses', pathMatch: 'full'
+      },
+      {
+        path: '**', component: Error404Component
+      }
+    ])
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/courses/course-info.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './course-info.component.html'
+})
+export class CourseInfoComponent {
+
+}
