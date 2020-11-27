8756bc32c92eb367b57c46bba1d9dc9c737dd34a
Leandro Toledo <leandrotoledo@hotmail.com.br>
2020-11-27 01:05:30 +0000

    modulo http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/course-list.component';
 import { StarComponent } from 'src/star/star.component';
@@ -22,12 +24,13 @@ import { CourseInfoComponent } from './courses/course-info.component';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule,
     RouterModule.forRoot([
       {
         path: 'courses', component: CourseListComponent
       },
       {
-        path: 'course/info', component: CourseInfoComponent
+        path: 'courses/info/:id', component: CourseInfoComponent
       },
       {
         path: '', redirectTo: 'courses', pathMatch: 'full'
